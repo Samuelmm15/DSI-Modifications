ccb143ac25e97f3088f92521d48ed25778dd942a
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-05-11 19:02:33 +0100

    Tested Mongodb

--- src/Course Notes/CourseNotesMongoDB.ts
+++ src/Course Notes/CourseNotesMongoDB.ts
@@ -1,14 +1,27 @@
-// import {MongoClient} from 'mongodb';
+import {MongoClient} from 'mongodb';
 
-// const dbURL = 'mongodb://127.0.0.1:27017';
-// const dbName = 'notes-app';
+const dbURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'notes-app';
 
-// MongoClient.connect(dbURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then((client) => {
-//   const db = client.db(dbName);
-//   console.log(db.databaseName);
-// }).catch((error) => {
-//   console.log(`Unable to connect to database: ${error.message}`);
-// });
+interface NoteInterface {
+  title: string,
+  body: string,
+  color: 'blue' | 'green' | 'red' | 'yellow' | 'magenta'
+}
+
+MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<NoteInterface>('notes').insertOne({
+    title: 'Red note',
+    body: 'This is a red note',
+    color: 'red',
+  });
+}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
